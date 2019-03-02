c6681814f41ffd2a41a8117915a16836a99e316d
TreverHibbs <treverhibbs@gmail.com>
2019-03-02 14:43:01 -0800

    added comments

--- main.c
+++ main.c
@@ -1,1 +1,22 @@
+/*
+Trever Hibbs
+Connor "lastname"
+"name"
 
+This program is meant to compute the maximum value that can be loaded into a 
+knapsack with a maximum weight capacity of 1024, when given a list of no more
+than 128 items with given numbers for value and weight. 
+The program excepts a single command line arguement specifying the max 
+weight capacity for the knapsack. Then once the program is exectuted it 
+will print out the contentse of the file knapsack.data, the maximum possible
+value, and how many of each item should be put in the bag. the program will
+then exit.
+
+*/
+
+#include"pa5.h"
+
+void main(int argv, char** argv){
+
+	
+} 
